7ce92882e423e1dd22f38de0ae3335c8fb65ebd4
Alejandro Cordero <carlos.eduardo17@itca.edu.sv>
2022-10-01 23:57:10 -0600

    Agregando Eliminar

--- models/libros/librosModel.php
+++ models/libros/librosModel.php
@@ -2,6 +2,7 @@
     class Libros_Model{
         private $db;
         private $libro;
+        
 
         public function __construct(){
             $this->db = Conectar::Conexion();
@@ -13,8 +14,17 @@
         public function create_libro($ISBN,$titulo,$fecha_publicacion,$autor,$categoria,$reseña,$edicion,$editorial){
             $result =$this->db->query("INSERT INTO libros(ISBN,Titulo,Fecha_publicacion,Autor,Categoria,Reseña,Edicion,Editorial) VALUES('$ISBN','$titulo','$fecha_publicacion','$autor','$categoria','$reseña','$edicion','$editorial')");
         }
+
+        public function delete_libro($id) { //ID
+            $query = "DELETE FROM libros where ID = ${id}";
+            $resultado = mysql_query($db, $query);
+    
+    
+        }
+    
     }
 
+     
 
 
 
