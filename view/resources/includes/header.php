ba8cdc92a586946a14121f0ed2c32f550c14dfe9
Jose Monterola <jmonterolad@gmail.com>
2022-10-12 15:15:48 -0600

    read, css and create changes

--- view/resources/includes/header.php
+++ view/resources/includes/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kindom Book</title>
-    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="view/css/header.css">
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     
@@ -16,9 +16,9 @@
         <a href="#" class="logo"><img src="view/resources/img/Logo Kingdom_Mesa de trabajo 1.png" alt="logo kindom book"></a>
         <div class="opciones">
             <ul class="navegacion">
-                <li><a href="#">INICIO</a></li>
-                <li><a href="#">CATEGORIAS</a></li>
-                <li><a href="#">INICIAR SESION</a></li>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="view/admin/books/crear.php">Agregar</a></li>
+                <li><a href="#">Administrar</a></li>
             </ul>
             <ion-icon name="menu-outline" class="menuToggle"></ion-icon>
         </div>
